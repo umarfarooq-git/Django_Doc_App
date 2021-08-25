b44d864d40ac2af657132bc55eabe1b7f1249fe6
umarfarooq-git <umaryousafkamboh@gmail.com>
2021-08-26 02:38:32 +0500

    Database creation and url routing

--- polls/apps.py
+++ polls/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'polls'
+class BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blog'
