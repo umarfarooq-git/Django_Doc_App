fe123b3464d34390702747ff2891562fd71bcc56
umarfarooq-git <umaryousafkamboh@gmail.com>
2021-08-27 05:06:32 +0500

    Working on admin side

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
+from django.utils import timezone
+import datetime
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+#class ChoiceInline(admin.StackedInline):  atack look, It takes more space at admin page.
+#   model = Choice
+#  extra = 3
+
+class ChoiceInline(admin.TabularInline):    #Tabular look at admin page
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question statement',               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    search_fields = ['question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    
+admin.site.register(Question,QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.contrib import admin
+
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+
+    @admin.display(             #display decorators contols the pattern and naming convening of forms containing data at admin side.
+    boolean=True,
+    ordering='pub_date',
+    description='Published recently?',
+    )
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
         return self.question_text
